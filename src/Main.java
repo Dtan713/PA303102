02e467d36e3b187721c1d1a6d091b6ddcb226ab0
Dtan713 <s.donaldtan@gmail.com>
2024-07-17 09:52:58 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 abstract class Book{
     String title;
     abstract void setTitle(String s);
+
     String getTitle(){
         return title;
     }
@@ -12,6 +13,7 @@ class MyBook extends Book {
     void setTitle(String s) {
         this.title = s;
     }
+
 }
 
 
@@ -26,7 +28,7 @@ public class Main{
         String title=sc.nextLine();
         MyBook new_novel=new MyBook();
         new_novel.setTitle(title);
-        System.out.println("The title is: +A title of two cities.getTitle");
+        System.out.println("The title is: "+new_novel.getTitle());
         sc.close();
 
     }
